2abdaca5688e8cd55e66738fecbf6b10f1021376
Sudhi Krishna N A <94775649+Sudhi27Krishna@users.noreply.github.com>
2022-12-08 18:05:44 +0530

    Update Leaf-Similar-Trees.java

--- Java/Trees/src/com/company/Leaf-Similar-Trees.java
+++ Java/Trees/src/com/company/Leaf-Similar-Trees.java
@@ -1,9 +1,24 @@
 // link => https://leetcode.com/problems/leaf-similar-trees/description/
 
-class Leaf-Similar-Trees {
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
     ArrayList<Integer> list1 = new ArrayList<>();
     ArrayList<Integer> list2 = new ArrayList<>();
-  
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
         traverse(root1, list1);
         traverse(root2, list2);
