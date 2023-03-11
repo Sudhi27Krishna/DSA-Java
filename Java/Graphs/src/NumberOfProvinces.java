0ba4cc960dad743c7dc80f853c5b615aa1980c34
Sudhi Krishna N A <94775649+Sudhi27Krishna@users.noreply.github.com>
2023-03-11 15:56:01 +0530

    Create  NumberOfProvinces.java

---
+++ Java/Graphs/src/NumberOfProvinces.java
@@ -0,0 +1,23 @@
+class  NumberOfProvinces {
+    public int findCircleNum(int[][] matrix) {
+        int n = matrix.length;
+        boolean[] vis = new boolean[n];
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            if(!vis[i]){
+                ans++;
+                dfs(matrix, vis, i);
+            }
+        }
+        return ans;
+    }
+
+    void dfs(int[][] matrix, boolean[] vis, int node){
+        vis[node] = true;
+        for(int i=0; i<matrix[node].length; i++){
+            if(matrix[node][i] == 1 && !vis[i]){
+                dfs(matrix, vis, i);
+            }
+        }
+    }
+}
