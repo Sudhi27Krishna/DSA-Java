4d0f3751c6795148acb8b129476615b95d61e71f
Sudhi Krishna N A <94775649+Sudhi27Krishna@users.noreply.github.com>
2023-05-09 12:11:49 +0530

    Create LongestSubArrayWithSumK.java

---
+++ Java/Sliding Window/LongestSubArrayWithSumK.java
@@ -0,0 +1,37 @@
+// GFG link => https://practice.geeksforgeeks.org/problems/longest-sub-array-with-sum-k0809/1
+
+class LongestSubArrayWithSumK{
+    // Function for finding maximum and value pair
+    public static int lenOfLongSubarr (int a[], int N, int k) {
+        int n = a.length; // size of the array.
+
+        Map<Long, Integer> preSumMap = new HashMap<>();
+        long sum = 0;
+        int maxLen = 0;
+        for (int i = 0; i < n; i++) {
+            //calculate the prefix sum till index i:
+            sum += a[i];
+
+            // if the sum = k, update the maxLen:
+            if (sum == k) {
+                maxLen = Math.max(maxLen, i + 1);
+            }
+
+            // calculate the sum of remaining part i.e. x-k:
+            long rem = sum - k;
+
+            //Calculate the length and update maxLen:
+            if (preSumMap.containsKey(rem)) {
+                int len = i - preSumMap.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            //Finally, update the map checking the conditions:
+            if (!preSumMap.containsKey(sum)) {
+                preSumMap.put(sum, i);
+            }
+        }
+
+        return maxLen;
+    }
+}
