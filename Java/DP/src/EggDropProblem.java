922a8928b193abbb70ed763449462f7c363cc96e
Sudhi Krishna N A <94775649+Sudhi27Krishna@users.noreply.github.com>
2023-09-12 16:57:26 +0530

    Update EggDropProblem.java

--- Java/DP/src/EggDropProblem.java
+++ Java/DP/src/EggDropProblem.java
@@ -32,5 +32,47 @@ public class EggDropProblem {
         dp[e][f] = result;
         return result;
     }
+
+
+    // Tabulation
+    public int solve(int A, int B) {
+        // A = eggs; B = floor;
+        int[][] dp = new int[A+1][B+1];
+        for(int i=0; i<=A; i++){
+            dp[i][0] = 1;
+            dp[i][1] = 1;
+        }
+        
+        for(int i=0; i<=B; i++) dp[1][i] = i;
+        
+        for(int e=2; e <= A; e++){
+            for(int f=2; f <= B; f++){
+                int min = Integer.MAX_VALUE;
+                for(int k=1; k<=f; k++){
+                    int temp = 1 + Math.max(dp[e-1][k-1], dp[e][f-k]);
+                    min = Math.min(min, temp);
+                }
+                dp[e][f] = min;
+            }
+        }
+        
+        
+        return dp[A][B];
+    }
+    
+    int fun(int e, int f, int[][] dp){
+        if(f == 0 || f == 1) return 1;
+        if(e == 1) return f;
+        
+        if(dp[e][f] != -1) return dp[e][f];
+        
+        int min = Integer.MAX_VALUE;
+        for(int k=1; k<=f; k++){
+            int temp = 1 + Math.max(fun(e-1, k-1, dp), fun(e, f - k, dp));
+            min = Math.min(min, temp);
+        }
+        
+        return dp[e][f] = min;
+    }
 }
 
