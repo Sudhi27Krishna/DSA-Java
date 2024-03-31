30cd4181ea5cb90fa23e97134d6b09df69a744fb
Sudhi Krishna N A <94775649+Sudhi27Krishna@users.noreply.github.com>
2024-03-31 12:36:10 +0530

    Update RBS.java

--- Java/binarysearch/src/com/company/RBS.java
+++ Java/binarysearch/src/com/company/RBS.java
@@ -22,4 +22,19 @@ class RBS {
 
         return -1;
     }
+
+    public int findMin(int[] nums) {
+        int n = nums.length;
+        int start = 0, end = n - 1;
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            int left = (mid - 1 + n) % n;
+            int right = (mid + 1) % n;
+            if(nums[left] >= nums[mid] && nums[mid] <= nums[right]) 
+                return nums[mid];
+            else if(nums[mid] <= nums[end]) end = mid - 1;
+            else if(nums[start] <= nums[mid]) start = mid + 1;
+        }
+        return nums[0];
+    }
 }
