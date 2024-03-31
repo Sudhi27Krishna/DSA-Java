8d53a99fa066ded7340c0e3b210a1431ea802836
Sudhi Krishna N A <94775649+Sudhi27Krishna@users.noreply.github.com>
2024-03-31 14:35:19 +0530

    Update Mountain.java

--- Java/binarysearch/src/com/company/Mountain.java
+++ Java/binarysearch/src/com/company/Mountain.java
@@ -6,19 +6,25 @@ public class Mountain {
         System.out.println(Peak(arr));
     }
     static int Peak(int[] a){
-        int start = 0;
-        int end = a.length - 1;
-        int mid;
-        while(start < end)
-        {
-            mid = start + (end-start)/2;
-            if(a[mid] > a[mid+1]){
-                end = mid;
+        int n = A.length;
+        int start = 0, end = n-1;
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            
+            if(mid > 0 && mid < n-1){
+                if(A[mid-1] > A[mid] && A[mid] < A[mid+1]) return A[mid];
+                else if(A[mid-1] < A[mid]) start = mid + 1;
+                else end = mid - 1;
             }
-            else {
-                start = mid + 1;
+            else if(mid == 0){
+                return A[mid] < A[mid+1] ? A[mid+1] : A[mid];
+            }
+            else{
+                return A[mid] < A[mid-1] ? A[mid-1] : A[mid];
             }
         }
-        return a[start]; // or return end; since start == end
+        
+        return A[end];
     }
 }
