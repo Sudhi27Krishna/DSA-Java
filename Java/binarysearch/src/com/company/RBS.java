ae878182cd343a6fa365f2eb408c064695cd77b3
Sudhi Krishna N A <94775649+Sudhi27Krishna@users.noreply.github.com>
2024-03-31 12:34:59 +0530

    Update RBS.java

--- Java/binarysearch/src/com/company/RBS.java
+++ Java/binarysearch/src/com/company/RBS.java
@@ -1,63 +1,25 @@
-package com.company;
+// Leetcode => https://leetcode.com/problems/search-in-rotated-sorted-array/description/
 
-public class RBS {
-    public static void main(String[] args) {
-       int[] arr = {4,5,6,7,0,1,2};
-        System.out.println(findPivot(arr));
-    }
-    static int binaryPivot(int[] a, int target){
-        int pivot = findPivot(a);
-//        if no pivot then do normal binary search
-        if(pivot == -1){
-            return binary(a,target,0,a.length - 1);
-        }
-        if(a[pivot] == target){
-            return target;
-        }
-        if(target >= a[0]){
-            return binary(a,target,0,pivot - 1);
-        }else{
-            return binary(a,target,pivot + 1,a.length - 1);
-        }
-    }
-    static int binary(int[] a,int target,int start,int end)
-    {
-        while(start<=end)
-        {
-            int mid = start + (end-start)/2;
-            if(target<a[mid])
-            {
-                end = mid - 1;
+class RBS {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int start = 0, end = n - 1; 
+
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] == target) return mid;
+
+            if(nums[start] <= nums[mid]){
+                if(nums[start] <= target && target <= nums[mid]) end = mid - 1;
+                else start = mid + 1;
             }
-            else if (target>a[mid])
-            {
-                start = mid + 1;
-            }
-            else
-            {
-                return mid;
+            else if(nums[mid] <= nums[end]){
+                if(nums[mid] <= target && target <= nums[end]) start = mid + 1;
+                else end = mid - 1;
             }
         }
-        return -1;
-    }
-    static int findPivot(int[] a){
-        int start = 0;
-        int end = a.length - 1;
-        while(start < end){
-            int mid = start + (end-start)/2;
-//            4 cases
-            if(mid < end && a[mid] > a[mid + 1]){
-                return mid;
-            }
-            if (mid > start && a[mid - 1] > a[mid]){
-                return mid -1;
-            }
-            if(a[mid] <= a[start]){
-                end = mid - 1;
-            }else{
-                start = mid + 1;
-            }
-        }
+
         return -1;
     }
 }
